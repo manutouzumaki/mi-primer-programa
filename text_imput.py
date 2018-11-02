8b8b41d83c0a787cefe576d7e31205ac0022f9b6
manutouzumaki <manuelcabrerizo5@gmail.com>
2018-11-02 00:45:51 -0300

    anadiendo el archivo hello

---
+++ text_imput.py
@@ -0,0 +1,39 @@
+
+number_to_guess = 2
+
+user_number = int(input("dime un numero"))
+
+if number_to_guess == user_number:
+    print("gano")
+else:
+    print("perido")
+    user_number = int(input("dime un numero"))
+
+    if number_to_guess == user_number:
+        print("gano")
+    else:
+        print("perdio")
+        user_number = int(input("dime un numero"))
+
+        if number_to_guess == user_number:
+            print("gano")
+        else:
+            print("perdio")
+            user_number = int(input("dime un numero"))
+
+            if number_to_guess == user_number:
+                print("gano")
+            else:
+                print("perdio")
+                user_number = int(input("dime un numero"))
+
+                if number_to_guess == user_number:
+                    print("gano")
+                else:
+                    print("perdio")
+
+
+
+
+
+
